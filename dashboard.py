f8fa3c67c59be2334c60d730db5f2c28010fe16e
RafiWidyansyah <61896896+RafiWidyansyah@users.noreply.github.com>
2025-10-28 22:35:29 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# Load Dataset
+data = pd.read_csv('coffee_shop_sales.csv')
+
+# Dashboard Configuration
+st.set_page_config(page_title="Coffee Sales Dashboard",
+                   page_icon="bar_chart:",
+                   layout="wide")
+
+## Filter Component
+min_date = data['transaction_date'].min()
+max_date = data['transaction_date'].max()
+location = data['store_location'].unique()
+product_cat = data['product_category'].unique()
+
+## Date Filter
+with st.sidebar:
+     st.sidebar.header("Date:")
+     start_date, end_date = st.date_input(label="Date Filter",
+                                          min_value=min_date,
+                                          max_value=max_date,
+                                          value=[min_date, max_date])
+
+## Location Filter
+with st.sidebar:
+     st.sidebar.header("Location:")
+     option = st.multiselect(label="Choose Location", location)
+                    
